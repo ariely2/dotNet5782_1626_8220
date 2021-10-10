58ad5bad7efaa10831ce61b6a2c75974ca014197
alon <alonzolti@gmail.com>
2021-10-10 18:55:32 +0300

    initial code + welcome functions

---
+++ targil0/Program8220.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome8220();
+            Welcome1626();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome1626();
+        private static void Welcome8220()
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application");
+        }
+    }
+}
