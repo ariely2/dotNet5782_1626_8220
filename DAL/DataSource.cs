eab40b406d548ab99d176d64e1af828541a22b0b
alonzolti <alonzolti@gmail.com>
2021-10-26 01:05:13 +0300

    initialization of Drones and Parcel. also add class of enum extension and the function randomEnumValue

--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -17,17 +17,40 @@ namespace DalObject
         internal class Config
         {
             static Random R = new Random();
-
+            static int IdOfParcel = 1;
             //some int?
             internal static void Initialize()
             {
+                ///initialize 10 customers
                 string[] CNames = new string[] { "Mario", "Carmen", "Tamar", "Daniel", "Adam", "Rosie", "Paul", "Jessie", "Leslie", "Charlie" };
                 for (int i = 0; i < 10; i++) /// Random Id between 1 and 1000000000. Name from Name array. Random phone number (adding zeroes in the front). Random Longitude between -180 and 80. Random Latitude between -90 and 90.
                     Customers.Add(new Customer() { Id = R.Next(1000000000), Name = CNames[i], Phone = R.Next(111111, 999999).ToString("000000"), Longitude = (R.NextDouble() * 260) - 180, Latitude = (R.NextDouble() * 180) - 90 });
+                
+                ///initialize 3 stations
                 string[] SNames = new string[] { "Florentin", "Mamilla", "Hulon" };
                 for (int i = 0; i < 3; i++) ///Random Id with 3 digits. Name from Name array. Random Longitude between -180 and 80. Random Latitude between -90 and 90. Random number of available slots between 0 and 3.
                     Stations.Add(new Station() { Id = R.Next(100, 1000), Name = SNames[i], Longitude = (R.NextDouble() * 260) - 180, Latitude = (R.NextDouble() * 180) - 90, ChargeSlots = R.Next(4) });
 
+                ///initialize 5 drones
+                //need to add Model Names
+                string[] MNames = new string[] { };
+                for (int i = 0; i < 5; i++)///Random Id with 5 digit. Name from Model Array. Random Max Weight. Random Status. Random battery between 0 to 100.
+                    Drones.Add(new Drone() { Id = R.Next(10000, 100000),Model = MNames[i], MaxWeight = EnumExtension.RandomEnumValue<WeightCategories>(),Status = EnumExtension.RandomEnumValue<DroneStatuses>() , Battery = R.Next(0, 101) });
+                
+                ///initialize 10 parcel
+                // need to add DateTime Variables: Delivered, PickedUp, Scheduled, Requested
+                for(int i = 0; i < 10; i++)
+                {
+                    int j = 0;
+                    WeightCategories ParcelMaxWeight = EnumExtension.RandomEnumValue<WeightCategories>();
+
+                    ///looking for an avaliable drone with enough battery and can carry the parcel
+                    for (; j < Drones.Count() && (Drones[j].Status != DroneStatuses.Avaliable || Drones[j].Battery == 0 || Drones[j].MaxWeight < ParcelMaxWeight); j++) ;
+
+                    ///Id equals to IdOfParcle. SenderId from customers list. TragetId from customers list. random max Weight. DroneId from list of Drones (only if there is exist such a drone). Random Priority. 
+                    Parcels.Add(new Parcel() { Id = IdOfParcel++, SenderId = Customers[R.Next(Customers.Count())].Id, TargetId = Customers[R.Next(Customers.Count())].Id,Weight = ParcelMaxWeight,DroneId = (j==5?0:Drones[j].Id),Priority = EnumExtension.RandomEnumValue<Priorities>()});
+                }
+                
             }
 
         }
--- DAL/Enum.cs
+++ DAL/Enum.cs
@@ -8,6 +8,15 @@ namespace IDAL
 {
     namespace DO
     {
+        public static class EnumExtension
+        {
+            static Random R = new Random();
+            public static T RandomEnumValue<T>()
+            {
+                var v = Enum.GetValues(typeof(T));
+                return (T)v.GetValue(R.Next(v.Length));
+            }
+        }
         public enum WeightCategories { Light, Medium, Heavy}
         public enum Priorities { Normal, Fast, Emergency}
         public enum DroneStatuses { Avaliable, Maintaince, Delivery}
