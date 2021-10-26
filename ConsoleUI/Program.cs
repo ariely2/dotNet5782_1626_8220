d7555879c6cb69aac9daa0974900b9d6377586f1
alonzolti <alonzolti@gmail.com>
2021-10-26 16:17:37 +0300

    program file

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,12 +1,176 @@
 ﻿using System;
-
+using IDAL.DalObject;
+using IDAL.DO;
 namespace ConsoleUI
 {
     class Program
     {
+        DalObject obj = new DalObject();
+        static void PrintGeneralOption()
+        {
+            Console.WriteLine(@"choose an option between 1 to 5:
+1)Add
+2)Update
+3)Display
+4)Display list
+5)exit");
+        }
+        static void PrintAddOption()
+        {
+            Console.WriteLine(@"choose an option between 1 to 4:
+1)Add staion
+2)Add Drone
+3)Add customer
+4)Add parcel");
+        }
+        static void PrintUpdateOption()
+        {
+            Console.WriteLine(@"choose an option between 1 to 5:
+1)Assign a parcel to a drone
+2)Pick up a parcel by a drone
+3)deliver a parcel to a customer
+4)send a drone to charge
+5)Release a drone from charging");
+        }
+        static void PrintDisplayOption()
+        {
+            Console.WriteLine(@"choose an option between 1 to 4:
+1)Display station
+2)Display drone
+3)Display customer
+4)Display parcel");
+        }
+        static void PrintDisplayListOption()
+        {
+            Console.WriteLine(@"choose an option between 1 to 6:
+1)Display stations list
+2)Display Drones list
+3)Display customers list
+4)Display parcels list
+5)Display parcels list that aren't associated with a drone
+6)Display stations list with avaliable charging spots");
+        }
+        static void SwitchAddOption() 
+        {
+            PrintAddOption();
+            int option;
+            Int32.TryParse(Console.ReadLine(), out option);
+            Console.Clear();
+            switch (option)
+            {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+            }
+        }
+        static void SwitchUpdateOption()
+        {
+            PrintUpdateOption();
+            int option;
+            Int32.TryParse(Console.ReadLine(), out option);
+            Console.Clear();
+            switch (option)
+            {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+                    break;
+            }
+        }
+        static void SwitchDisplayOption()
+        {
+            PrintDisplayOption();
+            int option;
+            Int32.TryParse(Console.ReadLine(), out option);
+            Console.Clear();
+            switch (option)
+            {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+
+            }
+        }
+        static void SwitchDisplayListOption()
+        {
+            PrintDisplayListOption();
+            int option;
+            Int32.TryParse(Console.ReadLine(), out option);
+            Console.Clear();
+            switch (option)
+            {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int option;
+            do
+            {
+                PrintGeneralOption();
+                Int32.TryParse(Console.ReadLine(), out option);
+                Console.Clear();
+                switch (option)
+                {
+                    case 1:
+                        SwitchAddOption();
+                        break;
+                    case 2:
+                        SwitchUpdateOption();
+                        break;
+                    case 3:
+                        SwitchDisplayOption();
+                        break;
+                    case 4:
+                        SwitchDisplayListOption();
+                        break;
+                    case 5:
+                        break;
+                }
+            } while (option != 5);
         }
     }
 }
--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -4,97 +4,99 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using IDAL.DO;
-
-namespace DalObject
+namespace IDAL
 {
-    public class DataSource
+    namespace DalObject
     {
-
-        internal static List<Customer> Customers = new List<Customer>(100);
-        internal static List<Parcel> Parcels = new List<Parcel>(1000);
-        internal static List<Drone> Drones = new List<Drone>(10);
-        internal static List<Station> Stations = new List<Station>(5);
-        internal class Config
+        public class DataSource
         {
-            static Random R = new Random();
-            static int IdOfParcel = 1;
-            internal static void Initialize()
+
+            internal static List<Customer> Customers = new List<Customer>(100);
+            internal static List<Parcel> Parcels = new List<Parcel>(1000);
+            internal static List<Drone> Drones = new List<Drone>(10);
+            internal static List<Station> Stations = new List<Station>(5);
+            internal class Config
             {
-                ///initialize 10 customers
-                string[] CNames = new string[] { "Mario", "Carmen", "Tamar", "Daniel", "Adam", "Rosie", "Paul", "Jessie", "Leslie", "Charlie" };
-                for (int i = 0; i < 10; i++)
-                    Customers.Add(new Customer()
-                    {
-                        Id = R.Next(1000000000),///Random Id between 1 and 1000000000
-                        Name = CNames[i],///Name from the array
-                        Phone = R.Next(111111, 999999).ToString("000000"),///Random phone number (adding zeroes in the front)
-                        Longitude = (R.NextDouble() * 260) - 180,///Random Longitude between -180 and 80
-                        Latitude = (R.NextDouble() * 180) - 90///Random latitude between -90 and 90
-                    });
-                
-                ///initialize 3 stations
-                string[] SNames = new string[] { "Florentin", "Mamilla", "Hulon" };
-                for (int i = 0; i < 3; i++)
-                    Stations.Add(new Station()
-                    {
-                        Id = R.Next(100, 1000),///Random Id with 3 digits
-                        Name = SNames[i],///Name from Name array.
-                        Longitude = (R.NextDouble() * 260) - 180,///Random Longitude between -180 and 80
-                        Latitude = (R.NextDouble() * 180) - 90,///Random Latitude between -90 and 90
-                        ChargeSlots = R.Next(4)///Random number of available slots between 0 and 3
-                    });
-
-                ///initialize 5 drones
-                //need to add Model Names
-                string[] MNames = new string[] { };
-                for (int i = 0; i < 5; i++)
-                    Drones.Add(new Drone()
-                    {
-                        Id = R.Next(10000, 100000),///Random Id with 5 digits
-                        Model = MNames[i],///Name from model array
-                        MaxWeight = EnumExtension.RandomEnumValue<WeightCategories>(),///Random MaxWeight
-                        Status = EnumExtension.RandomEnumValue<DroneStatuses>(),///Random Status
-                        Battery = R.Next(0, 101)///Random battery percentage between 0 and 100
-                    });
-
-
-                ///initialize 10 parcel
-                for (int i = 0; i < 10; i++)
+                static Random R = new Random();
+                static int IdOfParcel = 1;
+                internal static void Initialize()
                 {
-                    int j = 0;
-                    WeightCategories ParcelMaxWeight = EnumExtension.RandomEnumValue<WeightCategories>();
-                    ///looking for an avaliable drone with enough battery and can carry the parcel
-                    for (; j < Drones.Count() && (Drones[j].Status != DroneStatuses.Avaliable || Drones[j].Battery == 0 || Drones[j].MaxWeight < ParcelMaxWeight); j++) ;
-
-                    if (j != 5)
-                        Drones[j] = new Drone()
+                    ///initialize 10 customers
+                    string[] CNames = new string[] { "Mario", "Carmen", "Tamar", "Daniel", "Adam", "Rosie", "Paul", "Jessie", "Leslie", "Charlie" };
+                    for (int i = 0; i < 10; i++)
+                        Customers.Add(new Customer()
                         {
-                            Id = Drones[j].Id,
-                            Model = Drones[j].Model,///same Model
-                            Battery = Drones[j].Battery,///same Battery
-                            MaxWeight = Drones[j].MaxWeight,///same MaxWeight
-                            Status = DroneStatuses.Delivery///change the drone's status from avaliable to delivery
-                        };
+                            Id = R.Next(1000000000),///Random Id between 1 and 1000000000
+                            Name = CNames[i],///Name from the array
+                            Phone = R.Next(111111, 999999).ToString("000000"),///Random phone number (adding zeroes in the front)
+                            Longitude = (R.NextDouble() * 260) - 180,///Random Longitude between -180 and 80
+                            Latitude = (R.NextDouble() * 180) - 90///Random latitude between -90 and 90
+                        });
+
+                    ///initialize 3 stations
+                    string[] SNames = new string[] { "Florentin", "Mamilla", "Hulon" };
+                    for (int i = 0; i < 3; i++)
+                        Stations.Add(new Station()
+                        {
+                            Id = R.Next(100, 1000),///Random Id with 3 digits
+                            Name = SNames[i],///Name from Name array.
+                            Longitude = (R.NextDouble() * 260) - 180,///Random Longitude between -180 and 80
+                            Latitude = (R.NextDouble() * 180) - 90,///Random Latitude between -90 and 90
+                            ChargeSlots = R.Next(4)///Random number of available slots between 0 and 3
+                        });
+
+                    ///initialize 5 drones
+                    //need to add Model Names
+                    string[] MNames = new string[] { };
+                    for (int i = 0; i < 5; i++)
+                        Drones.Add(new Drone()
+                        {
+                            Id = R.Next(10000, 100000),///Random Id with 5 digits
+                            Model = MNames[i],///Name from model array
+                            MaxWeight = EnumExtension.RandomEnumValue<WeightCategories>(),///Random MaxWeight
+                            Status = EnumExtension.RandomEnumValue<DroneStatuses>(),///Random Status
+                            Battery = R.Next(0, 101)///Random battery percentage between 0 and 100
+                        });
 
 
-                    Parcels.Add(new Parcel()
+                    ///initialize 10 parcel
+                    for (int i = 0; i < 10; i++)
                     {
-                        Id = IdOfParcel++,///Id equals to IdOfParcle
-                        SenderId = Customers[R.Next(Customers.Count())].Id,///Random Id from customers list 
-                        TargetId = Customers[R.Next(Customers.Count())].Id,///Random Id from customers list, check that the sender is not the target
-                        Weight = ParcelMaxWeight,///Random MaxWeight
-                        DroneId = (j == 5 ? 0 : Drones[j].Id),///Id from drones's list (only if there is a drone that match the requirements)
-                        Priority = EnumExtension.RandomEnumValue<Priorities>(),///Random priority
-                        Requested = DateTime.Now,///the person requested the parcel now
-                        Scheduled = (j == 5 ? DateTime.MinValue : DateTime.Now),///time that we assign a drone to the parcel, MinValue - if we didn't find a drone
-                        PickedUp = (j == 5 ? DateTime.MinValue : DateTime.Now),//not sure about the value.
-                        Delivered = DateTime.MinValue///the drone didn't delivered the pacel yet, so the value is MinValue
-                    });
+                        int j = 0;
+                        WeightCategories ParcelMaxWeight = EnumExtension.RandomEnumValue<WeightCategories>();
+                        ///looking for an avaliable drone with enough battery and can carry the parcel
+                        for (; j < Drones.Count() && (Drones[j].Status != DroneStatuses.Avaliable || Drones[j].Battery == 0 || Drones[j].MaxWeight < ParcelMaxWeight); j++) ;
+
+                        if (j != 5)
+                            Drones[j] = new Drone()
+                            {
+                                Id = Drones[j].Id,
+                                Model = Drones[j].Model,///same Model
+                                Battery = Drones[j].Battery,///same Battery
+                                MaxWeight = Drones[j].MaxWeight,///same MaxWeight
+                                Status = DroneStatuses.Delivery///change the drone's status from avaliable to delivery
+                            };
+
+
+                        Parcels.Add(new Parcel()
+                        {
+                            Id = IdOfParcel++,///Id equals to IdOfParcle
+                            SenderId = Customers[R.Next(Customers.Count())].Id,///Random Id from customers list 
+                            TargetId = Customers[R.Next(Customers.Count())].Id,///Random Id from customers list, check that the sender is not the target
+                            Weight = ParcelMaxWeight,///Random MaxWeight
+                            DroneId = (j == 5 ? 0 : Drones[j].Id),///Id from drones's list (only if there is a drone that match the requirements)
+                            Priority = EnumExtension.RandomEnumValue<Priorities>(),///Random priority
+                            Requested = DateTime.Now,///the person requested the parcel now
+                            Scheduled = (j == 5 ? DateTime.MinValue : DateTime.Now),///time that we assign a drone to the parcel, MinValue - if we didn't find a drone
+                            PickedUp = (j == 5 ? DateTime.MinValue : DateTime.Now),//not sure about the value.
+                            Delivered = DateTime.MinValue///the drone didn't delivered the pacel yet, so the value is MinValue
+                        });
+
+                    }
 
                 }
 
             }
-
         }
     }
 }
