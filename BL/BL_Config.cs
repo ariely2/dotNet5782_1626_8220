e1bd8ce83e9bdbddf22abf6b8170420a8c747d10
ariely2 <arielojelo@gmail.com>
2021-11-15 18:32:41 +0200

    added constructor

--- BL/BL_Config.cs
+++ BL/BL_Config.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IDAL;
+using IDAL.DalObject;
 
 namespace IBL.BO
 {
-    public partial class BO:IBL
+    public partial class BL:IBL
     {
+        IDal DataMethods;
+        public static double AvailableUse; 
+        public static double LightUse; 
+        public static double MediumUse; 
+        public static double HeavyUse;
+        public static double ChargeRate;
         
+        public BL() 
+        { 
+            DataMethods = new DalObject();
+            double[] info = DataMethods.GetBatteryUsageInfo();
+            AvailableUse = info[0];
+            LightUse = info[1];
+            MediumUse = info[2];
+            HeavyUse = info[3];
+            ChargeRate = info[4];
+
+        }
     }
 }
