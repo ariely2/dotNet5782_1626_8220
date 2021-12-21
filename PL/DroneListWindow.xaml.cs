28c72ed2818a66b25d3f0b7b9d9a6e3a800d188c
ariely2 <arielojelo@gmail.com>
2021-12-21 17:02:14 +0200

    added combobox, button, drone list view

--- PL/DroneListWindow.xaml.cs
+++ PL/DroneListWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using IBL;
 
 namespace PL
 {
@@ -19,9 +20,48 @@ namespace PL
     /// </summary>
     public partial class DroneListWindow : Window
     {
+        private IBL.IBL bl;
         public DroneListWindow()
         {
             InitializeComponent();
+        }
+        public DroneListWindow(IBL.IBL b)
+        {
+            bl = b;
+            InitializeComponent();
+            StatusSelector.Items.Add("All");
+            var statuses  = Enum.GetValues(typeof(IBL.BO.DroneStatuses));
+            foreach (var a in statuses)
+                StatusSelector.Items.Add(a);
+            WeightSelector.Items.Add("All");
+            var weights = Enum.GetValues(typeof(IBL.BO.WeightCategories));
+            foreach (var a in weights)
+                WeightSelector.Items.Add(a);
+            DronesListView.ItemsSource = bl.RequestList<IBL.BO.DroneToList>();
+        }
+        private void StatusSelection(object sender, SelectionChangedEventArgs e)//grey out options that don't exist?
+        {
+            var a = StatusSelector.SelectedItem;
+            if (a.GetType() == typeof(string))
+                DronesListView.ItemsSource = bl.RequestList<IBL.BO.DroneToList>();
+            else
+            {
+                var b = DronesListView.ItemsSource as List<IBL.BO.DroneToList>;
+                DronesListView.ItemsSource = b.FindAll(x => x.Status == (IBL.BO.DroneStatuses)a);
+            }
+        }
+                //DronesListView.ItemsSource = bl.RequestList<IBL.BO.DroneToList>().ToList().FindAll(x => x.Status == (IBL.BO.DroneStatuses)a);
+        private void WeightSelection(object sender, SelectionChangedEventArgs e)//fix so you can use both selectors at same time
+        {
+            var a = WeightSelector.SelectedItem;
+            if (a.GetType() == typeof(string))
+                DronesListView.ItemsSource = bl.RequestList<IBL.BO.DroneToList>();
+            else
+            {
+                var b = DronesListView.ItemsSource as List<IBL.BO.DroneToList>;
+                DronesListView.ItemsSource = b.FindAll(x => x.MaxWeight == (IBL.BO.WeightCategories)a);
+            }    
+                //DronesListView.ItemsSource = bl.RequestList<IBL.BO.DroneToList>().ToList().FindAll(x => x.MaxWeight == (IBL.BO.WeightCategories)a);
         }
     }
 }
