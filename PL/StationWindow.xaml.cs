b2deddf6bc649608ce30a9083f512e15b9ed7813
alonzolti <alonzolti@gmail.com>
2022-02-02 20:22:39 +0200

    small cahnges

--- PL/StationWindow.xaml.cs
+++ PL/StationWindow.xaml.cs
@@ -65,6 +65,7 @@ namespace PL
                 MessageBox.Show(errorMessage);
                 return;
             }
+            
             try
             {
                 bl.Create<BO.Station>(station);
